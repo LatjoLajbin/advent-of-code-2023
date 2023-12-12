a3f892d8579eb3bd2f3382306422c0cb28554db6
Albin N <albin.nilsson@nexergroup.com>
2023-12-12 23:28:04 +0100

    Closer towards 07

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,26 +1,101 @@
-use itertools::Itertools;
+use core::hash::Hash;
+use std::collections::HashSet;
 
 advent_of_code::solution!(7);
 
+#[repr(u32)]
+#[derive(PartialEq, Eq)]
+enum Card {
+    A = 14,
+    K = 13,
+    Q = 12,
+    J = 11,
+    T = 10,
+    Num(u32) = 9,
+}
+
+impl Card {
+    fn value(&self) -> u32 {
+        use crate::Card::*;
+        match *self {
+            A | K | Q | J | T => unsafe { *(self as *const Self as *const u32) },
+            Num(num) => {
+                if num >= 2 && num <= 9 {
+                    num
+                } else {
+                    panic!()
+                }
+            }
+        }
+    }
+}
+
+// TODO(ooooooooooooo.......)
+impl PartialEq for MyEnum {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (MyEnum::Var3(a, _), MyEnum::Var3(b, _)) => a == b,
+            _ => format!("{:?}", self) == format!("{:?}", other),
+        }
+    }
+}
+
+// TODO(ooooooooooooo.......)
+impl Hash for Card {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        match self {
+            MyEnum::Var3(a, _) => a.hash(state),
+            _ => format!("{:?}", self).hash(state),
+        }
+    }
+}
+
 enum Type {
-    FiveKind,
-    FourKind,
-    FullHouse,
-    ThreeKind,
-    TwoPair,
-    Pair,
-    HighCard,
+    FiveKind(u32),
+    FourKind(u32),
+    FullHouse(u32, u32),
+    ThreeKind(u32),
+    TwoPair(u32, u32),
+    Pair(u32),
+    HighCard(u32),
 }
 struct Hand {
-    cards: [char; 5],
+    cards: [Card; 5],
+    cards_set: HashSet<Card>,
 }
 
 impl Hand {
-    fn Type(&self) -> Type {
-        for pair in self.cards.into_iter().combinations(2) {}
+    fn build(cards: [Card; 5]) -> Hand {
+        let set = cards.iter().fold(HashSet::new(), |mut acc, card| {
+            acc.insert(*card);
+            acc
+        });
+
+        Hand {
+            cards,
+            cards_set: set,
+        }
+    }
+
+    fn hand_type(&self) -> Type {
+        let amount_of_unique_values = self.cards_set.len();
+        let cards_clone = self.cards.clone();
+        let highest_value = cards_clone.iter().max().unwrap();
+        let second_highest_value = cards_clone
+            .iter()
+            .filter(|card| *card != highest_value)
+            .max()
+            .unwrap_or(&'0');
+
+        if amount_of_unique_values == 1 {
+            Type::HighCard
+        } else if amount_of_unique_values == 2 {
+            Type::FourKind
+        }
 
         Type::HighCard
     }
+
     fn five_kind(&self) -> bool {
         match self.cards {
             [head, tail @ ..] => tail.iter().all(|x| *x == head),
